3d632b8bb301a69443d89891b48bfbc46163e86d
Jan Bílek <honza@elision.design>
2019-04-04 22:16:38 +0200

    Update packages

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,11 @@
         <span class="font-weight-light">MATERIAL DESIGN</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
+      <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
         <span class="mr-2">Latest Release</span>
         <v-icon>open_in_new</v-icon>
       </v-btn>
     </v-toolbar>
-
     <v-content>
       <HelloWorld/>
     </v-content>
