0a964d3c198c77a375ea16671dcde46d3b0f3a0e
Jan Bílek <honza@elision.design>
2019-04-04 22:59:16 +0200

    Add lazy route

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Dashboard from '@/pages/Dashboard.vue';
 import LayoutBase from '@/layouts/LayoutBase.vue';
-// import LayoutCentered from '@/layouts/LayoutCentered.vue';
+import LayoutCentered from '@/layouts/LayoutCentered.vue';
 
 Vue.use(Router);
 
@@ -19,14 +19,14 @@ export default new Router({
         component: Dashboard,
       }],
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   component: LayoutCentered,
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    // },
+    {
+      path: '/about',
+      name: 'about',
+      component: LayoutCentered,
+      children: [{
+        path: '',
+        component: () => import(/* webpackChunkName: "about" */ '@/pages/Dashboard.vue'),
+      }],
+    },
   ],
 });
